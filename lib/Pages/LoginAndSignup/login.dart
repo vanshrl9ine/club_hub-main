f7caac558d8c4ddfdd8d64ed1a8b85ff6e46fb5b
Vansh <gu[ptavansh67@gmail.com>
2023-12-01 02:05:49 +0530

    google login removed

--- lib/Pages/LoginAndSignup/login.dart
+++ lib/Pages/LoginAndSignup/login.dart
@@ -174,7 +174,7 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(
                   height: 20,
                 ),
-                googleLogin(context, size),
+
                 SizedBox(height: size.height * 0.15),
                 Container(
                   color: const Color.fromARGB(255, 62, 62, 62),
@@ -212,96 +212,5 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  ElevatedButton googleLogin(BuildContext context, Size size) {
-    return ElevatedButton(
-      onPressed: () async {
-        if (context.mounted) {
-          showDialog(
-            context: context,
-            barrierDismissible: true,
-            builder: (BuildContext context) {
-              return const Center(child: CircularProgressIndicator());
-            },
-          );
-        }
-        String val = await Auth.googleLogin();
-        if (context.mounted) Navigator.pop(context);
-        if (val == 'success') {
-          String ptype = await FirebaseFirestore.instance
-              .collection('users')
-              .doc(FirebaseAuth.instance.currentUser!.uid)
-              .get()
-              .then((value) => value.data()!['profileType'].toString());
-          if (context.mounted) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => HomePage(
-                  currentIndex: 0,
-                  profileType: ptype,
-                ),
-              ),
-            );
-          }
-          val = 'Signed in';
-        } else if (val == 'first') {
-          String ptype = await FirebaseFirestore.instance
-              .collection('users')
-              .doc(FirebaseAuth.instance.currentUser!.uid)
-              .get()
-              .then((value) => value.data()!['profileType'].toString());
 
-          if (context.mounted) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => HomePage(
-                  currentIndex: 1,
-                  profileType: ptype,
-                ),
-              ),
-            );
-          }
-          val = 'Signed in';
-        }
-        scaffoldMessenger.showSnackBar(
-          SnackBar(
-            content: Text(val.toString()),
-          ),
-        );
-      },
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(50),
-        ),
-        backgroundColor: const Color(0xFF4E60FF),
-        elevation: 8,
-        padding: const EdgeInsets.symmetric(vertical: 10),
-      ),
-      child: const SizedBox(
-        width: 250,
-        height: 25,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              FontAwesomeIcons.google,
-              color: Colors.white,
-            ),
-            SizedBox(width: 5),
-            Text(
-              '  Continue with Google',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontFamily: 'Inter',
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
